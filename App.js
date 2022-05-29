2f6eebbc8390d0bf9e409428d46e08463c4f6429
Atif <atifonat@gmail.com>
2022-05-29 16:47:11 +1000

    Login validation implemented

--- App.js
+++ App.js
@@ -4,36 +4,63 @@ import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-n
 import { fetchApi } from './services/'
 
 export default function App() {
-  const [email, setEmail] = useState("...");
-  const [password, setPassword] = useState("...");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErrMsg, setEmailErrMsg] = useState("");
+  const [passwordErrMsg, setPasswordErrMsg] = useState("");
+  const [summaryErrMsg, setSummaryErrMsg] = useState("");
+  const [token, setToken] = useState("");
 
   const onLoginRequest = () => {
-    fetchApi.tokens.getJWT(email, password);
+    if(validLoginDetails(email, password)){
+      fetchApi.tokens.getJWT({email, password})
+      .then(data => {
+        if(data.error){
+          setSummaryErrMsg("email and password combination not recognised");
+          return;
+        }
+        setToken(data);
+      });
+    }
+
+  };
+
+  const validLoginDetails = (email, password) => {
+    let regE = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    setEmailErrMsg(regE.test(email) === false ? "invalid email, please try again" : "");
+
+    let regP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])(?=.*[a-zA-Z]).{6,}$/;
+    setPasswordErrMsg(regP.test(password) === false 
+      ? "invalid password, please try again" : "");
+
+    return regE.test(email) && regP.test(password);
   };
   
   return (
     <SafeAreaView style={styles.container}>
 
+      <Text>{summaryErrMsg}</Text>
       <TextInput
         style={styles.input}
         onChangeText={(e) => {setEmail(e)}}
         placeholder='youremail@gmail.com'
-        keyboardType='email-address'
-      />
+        keyboardType='email-address' />
+      <Text>{emailErrMsg}</Text>
 
       <TextInput
         style={styles.input}
         onChangeText={(e) => setPassword(e)}
         placeholder='yourpassword'
-      />
-
-      <Text>{email}</Text>
-      <Text>{password}</Text>
+        secureTextEntry={false} />
+      <Text>{passwordErrMsg}</Text>
 
       <Button
         title='Login'
         color="#841584"
         onPress={onLoginRequest} /> 
+        
+      <Text>{token}</Text>
+
     </SafeAreaView>
   );
 }
--- services/api/tokens.js
+++ services/api/tokens.js
@@ -1,8 +1,20 @@
 let baseUrl = "https://localhost:7271/api/tokens";
 
-const getJWT = async (email, password) => {
-    return await fetch(`${baseUrl}`)
-        .then(response => response.json());
+const getJWT = async (logInDto) => {
+    return await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(logInDto)
+    })
+    .then(response => {     
+        if(response.ok){
+            return response.json();
+        }       
+        return {...response, error:true} ;              
+    });
 };
 
 export { getJWT }; 
